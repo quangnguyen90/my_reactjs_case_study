0a89d9eca35911c6faec15ecb485178c0aa2df3e
quangnguyen90 <nguyenphuquang90@gmail.com>
2020-06-23 21:31:36 +0700

    Apply module classnames

--- src/components/Todo/TodoItem.js
+++ src/components/Todo/TodoItem.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
 import './TodoItem.css';
 
 class TodoItem extends Component {
     render() {
         const { item } = this.props;
-        let className = 'TodoItem';
-        if (item.isComplete) {
-            className += ' TodoItem-complete';
-        }
         return (
-            <div className={className}>
+            <div className={classNames('TodoItem', {
+                'TodoItem-complete': item.isComplete
+            })}>
                 <p>{this.props.item.title}</p>
             </div>
         )
