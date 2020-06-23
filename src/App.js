dca873bffcab6422df4fe3dfc8ee06009690e878
quangnguyen90 <nguyenphuquang90@gmail.com>
2020-06-23 20:06:24 +0700

    Improve traffic light

--- src/App.js
+++ src/App.js
@@ -4,22 +4,48 @@ import './App.css';
 import TodoItem from './components/Todo/TodoItem';
 import TraffictLight from './components/TrafficLight/TrafficLight';
 
+const RED = 0;
+const ORANGE = 1;
+const GREEN = 2;
+
 class App extends Component {
   constructor() {
     super();
-    this.todoItems = [
-      { title: 'PHP', isComplete: true },
-      { title: 'MySQL', isComplete: true },
-      { title: 'NodeJS' },
-      { title: 'Mongo DB' },
-      { title: 'ReactJS' },
-      { title: 'React Hook' },
-      { title: 'Redux-thunk' },
-      { title: 'Redux-Saga' },
-    ];
+    // this.todoItems = [
+    //   { title: 'PHP', isComplete: true },
+    //   { title: 'MySQL', isComplete: true },
+    //   { title: 'NodeJS' },
+    //   { title: 'Mongo DB' },
+    //   { title: 'ReactJS' },
+    //   { title: 'React Hook' },
+    //   { title: 'Redux-thunk' },
+    //   { title: 'Redux-Saga' },
+    // ];
+
+    this.state = {
+      currentColor: RED
+    };
+
+    setInterval(() => {
+      this.setState({
+        currentColor: this.getNextColor(this.state.currentColor)
+      });
+    }, 3000);
+  }
+
+  getNextColor(color) {
+    switch (color) {
+      case RED:
+        return ORANGE
+      case ORANGE:
+        return GREEN;
+      default:
+        return RED;
+    }
   }
 
   render() {
+    const { currentColor } = this.state;
     return (
       // <div className="App">
       //   {this.todoItems.length > 0 &&
@@ -30,7 +56,7 @@ class App extends Component {
       //   {this.todoItems.length === 0 && 'Nothing here'}
       // </div>
 
-      <TraffictLight />
+      <TraffictLight currentColor={currentColor} />
     );
   }
 
--- src/components/TrafficLight/TrafficLight.js
+++ src/components/TrafficLight/TrafficLight.js
@@ -7,33 +7,9 @@ const ORANGE = 1;
 const GREEN = 2;
 
 class TraffictLight extends Component {
-    constructor() {
-        super();
-        this.state = {
-            currentColor: RED
-        };
-
-        setInterval(() => {
-            this.setState({
-                currentColor: this.getNextColor(this.state.currentColor)
-            });
-        }, 3000);
-    }
-
-    getNextColor(color) {
-        switch (color) {
-            case RED:
-                return ORANGE
-            case ORANGE:
-                return GREEN;
-            default:
-                return RED;
-        }
-    }
-
     render() {
         console.log('Rendering...');
-        const { currentColor } = this.state;
+        const { currentColor } = this.props;
         return (
             <div className="TrafficLight">
                 <div className={classNames('blub', 'red', {
