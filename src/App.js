e04699ad594f4cb3e04a41efbc5ba5b58aac276e
quangnguyen90 <nguyenphuquang90@gmail.com>
2020-06-23 18:42:09 +0700

    Dynamic class name

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoItem from './components/TodoItem';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.todoItems = [
+      { title: 'Lam viec 1', isComplete: true },
+      { title: 'Lam viec 2', isComplete: true },
+      { title: 'Lam viec 3' },
+    ];
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.todoItems.map((item, index) =>
+            <TodoItem key={index} item={item} />
+          )
+        }
+      </div>
+    );
+  }
+
 }
 
 export default App;
