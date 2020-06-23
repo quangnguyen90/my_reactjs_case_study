4237eb7652691746916c0f7c6123d9857836587f
quangnguyen90 <nguyenphuquang90@gmail.com>
2020-06-23 20:40:07 +0700

    Back to Todo App

--- src/App.js
+++ src/App.js
@@ -2,61 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TodoItem from './components/Todo/TodoItem';
-import TraffictLight from './components/TrafficLight/TrafficLight';
-
-const RED = 0;
-const ORANGE = 1;
-const GREEN = 2;
 
 class App extends Component {
   constructor() {
     super();
-    // this.todoItems = [
-    //   { title: 'PHP', isComplete: true },
-    //   { title: 'MySQL', isComplete: true },
-    //   { title: 'NodeJS' },
-    //   { title: 'Mongo DB' },
-    //   { title: 'ReactJS' },
-    //   { title: 'React Hook' },
-    //   { title: 'Redux-thunk' },
-    //   { title: 'Redux-Saga' },
-    // ];
-
-    this.state = {
-      currentColor: RED
-    };
-
-    setInterval(() => {
-      this.setState({
-        currentColor: this.getNextColor(this.state.currentColor)
-      });
-    }, 3000);
+    this.todoItems = [
+      { title: 'PHP', isComplete: true },
+      { title: 'MySQL', isComplete: true },
+      { title: 'NodeJS' },
+      { title: 'Mongo DB' },
+      { title: 'ReactJS' },
+      { title: 'React Hook' },
+      { title: 'Redux-thunk' },
+      { title: 'Redux-Saga' },
+    ];
   }
-
-  getNextColor(color) {
-    switch (color) {
-      case RED:
-        return ORANGE
-      case ORANGE:
-        return GREEN;
-      default:
-        return RED;
-    }
-  }
-
   render() {
-    const { currentColor } = this.state;
     return (
-      // <div className="App">
-      //   {this.todoItems.length > 0 &&
-      //     this.todoItems.map((item, index) => (
-      //       <TodoItem key={index} item={item} />
-      //     ))
-      //   }
-      //   {this.todoItems.length === 0 && 'Nothing here'}
-      // </div>
-
-      <TraffictLight currentColor={currentColor} />
+      <div className="App">
+        {this.todoItems.length > 0 &&
+          this.todoItems.map((item, index) => (
+            <TodoItem key={index} item={item} />
+          ))
+        }
+        {this.todoItems.length === 0 && 'Nothing here'}
+      </div>
     );
   }
 
