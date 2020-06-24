1fcab86274e04f25bca05b34084a6caa232b0634
quangnp <z.quang.nguyenphu@gree.net>
2020-06-24 15:59:25 +0700

    Props Type

--- src/components/Todo/TodoItem.js
+++ src/components/Todo/TodoItem.js
@@ -1,5 +1,8 @@
+
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
+
 import checkImg from '../../img/check.svg';
 import checkCompleteImg from '../../img/check-complete.svg';
 import './TodoItem.css';
@@ -15,11 +18,25 @@ class TodoItem extends Component {
             <div className={classNames('TodoItem', {
                 'TodoItem-complete': item.isComplete
             })}>
-                <img onClick={onClick} src={url} width={32} height={32} />
+                <img
+                    alt="Check"
+                    onClick={onClick}
+                    src={url}
+                    width={32}
+                    height={32}
+                />
                 <p>{this.props.item.title}</p>
             </div>
         )
     }
+}
+
+TodoItem.propTypes = {
+    item : PropTypes.shape({
+        isComplete: PropTypes.bool.isRequired,
+        title: PropTypes.string.isRequired
+    }),
+    onClick : PropTypes.func.isRequired
 }
 
 export default TodoItem;
